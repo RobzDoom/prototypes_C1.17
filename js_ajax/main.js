73f228de2b0052e1f7502379e09fbd33851473ba
RobzDoom <jorgecruz.rd@gmail.com>
2017-01-31 15:08:20 -0800

    delete all code except comments

--- js_ajax/main.js
+++ js_ajax/main.js
@@ -9,9 +9,23 @@ $(document).ready(function(){
             url: 'http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topMovies/json',
             success: function(result) {
                 console.log('AJAX Success function called, with the following result:', result);
+                global_result = result;
 
+                var arrivalMovie = console.log(global_result.feed.entry[0]);
+                var arrivalMovieThirdImage = console.log(global_result.feed.entry[0]['im:image'][2].label);
             }
         });
         console.log('End of click function');
     });
 });
+
+var global_result =  null;
+
+// var arrivalMovie = console.log(global_result.feed.entry[0]);
+//
+// var arrivalMovieThirdImage = console.log(global_result.feed.entry[0]['im:image'][2].label);
+
+
+
+
+
