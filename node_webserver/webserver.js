1f91df4d7cc123dc27172e7b0efe8a52bfc5b9e7
RobzDoom <jorgecruz.rd@gmail.com>
2017-02-20 12:17:29 -0800

    finishes 0.1

--- node_webserver/webserver.js
+++ node_webserver/webserver.js
@@ -1,1 +1,11 @@
+var http = require('http');
 
+var server = http.createServer(function(request, response){
+      console.log('handling request');
+      response.write('initializing request response \n');
+      response.end("This server is responding webserver");
+    });
+
+    server.listen(3000,function(){
+          console.log("Server listening on: http://localhost:3000");
+        });
